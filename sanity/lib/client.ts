7d9c68ee4f6441d55aa50b6ba6176854474dbea8
apna-colleg <hemashrivastava06@gmail.com>
2026-02-16 20:16:02 +0530

    Update dependencies and enhance Sanity client configuration: added mapbox-gl and react-map-gl to package.json, updated pnpm-lock.yaml, and modified the Sanity client to include a read client and improved API version handling.

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,18 @@
 import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from "../env";
+import {dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  apiVersion: "2025-01-01",
+  useCdn: process.env.NODE_ENV === "production",
+  token: process.env.SANITY_API_TOKEN // Set to false if statically generating pages, using ISR or tag-based revalidation
+});
+
+export const readClient = createClient({
+  projectId,
+  dataset,
+  apiVersion: "2025-01-01",
+  useCdn: true,
 });
