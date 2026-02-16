2d9fb6336d766210b30d924544dd1dd91a4bc32b
apna-colleg <hemashrivastava06@gmail.com>
2026-02-16 19:50:59 +0530

    Enhance configuration and dependencies: added remote image patterns for Sanity, updated package.json with new dependencies including @hookform/resolvers and @radix-ui/react-slot, and improved input, button, and badge components with better styling and structure.

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -7,4 +7,19 @@ import { defineCliConfig } from "sanity/cli";
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
-export default defineCliConfig({ api: { projectId, dataset } });
+export default defineCliConfig({
+  api: {
+    projectId,
+    dataset,
+  },
+  // Configuration for `sanity typegen generate`
+  typegen: {
+    // Look for GROQ queries in app/lib (including `lib/sanity/queries.ts`)
+    path: "./{app,components,lib,sanity,hooks,utils,actions}/**/*.{ts,tsx",
+    // Uses the schema extracted by `sanity schema extract --path=./schema.json`
+
+    // Where to write the generated types
+    generates: "./sanity.types.ts",
+
+  },
+});
